7a970d3ff50081aee46f8b52ed538852b5fac8a5
gbs4ever <gbs4ever2@gmail.com>
2019-03-09 20:47:39 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,42 @@
 class TweetsController < ApplicationController
+get '/tweets' do
+    @tweets = Tweet.all 
+    erb :'tweets/tweets'
+end  
+    
+
+get '/tweets/new' do
+    erb :'tweets/new'
+
+end 
+
+post '/tweets' do
+    tweet = Tweet.create(content: params[:content])
+redirect "/tweets/#{tweet.id}"
+end  
+
+get '/tweets/:id' do 
+    @tweet = Tweet.find_by(params[:id])
+    erb :'/tweets/show_tweet'  
+end 
+
+get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by(params[:id])
+    binding.pry  
+    erb :'tweets/edit_tweet'
+end 
+
+patch '/tweets/:id' do
+    binding.pry  
+
+redirect "/tweets/#{tweet.id}"
+end 
+
+
+
+
+
+
 
 
 end
